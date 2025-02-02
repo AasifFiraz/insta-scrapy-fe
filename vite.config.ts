6cbb9264bd4c360c3bdfd578e947082d40760dc5
Aarthif-N <aarthif@threadlet.com.au>
2025-02-02 18:41:35 +1100

    [FIX] - frontend port

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       host: true,
-      port: 3001,
+      port: 3000,
       proxy: {
         '/api': {
           target: API_URL,
