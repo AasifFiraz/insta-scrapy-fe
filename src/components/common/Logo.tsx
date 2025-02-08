82269d30a94d7f44b129f60c10d36e4f180cf213
Aarthif-N <aarthif@threadlet.com.au>
2025-02-08 15:04:32 +1100

    [FIX] - Include logo

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { LineChart } from 'lucide-react';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogoIcon from '../../utils/Postlyze.png'
 
 export const Logo: React.FC = () => {
   return (
     <Link to="/" className="flex items-center gap-2 hover:opacity-90 transition-opacity">
-      <div className="bg-red-500 p-1.5 rounded">
-        <LineChart className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
+      <div className="">
+        <img 
+          src={LogoIcon} 
+          alt="Logo" 
+          className="w-10 h-10" // Adjust sizing as needed
+        />
       </div>
       <span className="text-white font-bold text-lg sm:text-xl">POSTLYZE</span>
     </Link>
