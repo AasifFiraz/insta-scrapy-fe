9f5536e7631fcd34deaac630ad1d689b834f18c9
Aarthif-N <aarthif@threadlet.com.au>
2025-05-10 23:27:12 +1000

    [FIX] - Include Icons

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoIcon from '../../utils/Postlyze.png'
+import LogoIcon from "../../utils/Postlyze.png";
 
 export const Logo: React.FC = () => {
   return (
-    <Link to="/" className="flex items-center gap-2 hover:opacity-90 transition-opacity">
-      <div className="">
-        <img 
-          src={LogoIcon} 
-          alt="Logo" 
-          className="w-10 h-10" // Adjust sizing as needed
+    <Link
+      to="/"
+      className="flex items-center gap-2 hover:opacity-90 transition-opacity"
+    >
+      <div>
+        <img
+          src={LogoIcon}
+          alt="Logo"
+          className="w-10 h-10 rounded-full object-cover"
         />
       </div>
       <span className="text-white font-bold text-lg sm:text-xl">POSTLYZE</span>
